545cd3df2a8a9e27aa595e0381e80ba3c3aa1d5a
amanxsays <amanjhasahab07@gmail.com>
2025-06-28 14:07:11 +0530

    fully setup the firebase deploy

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,12 +21,12 @@ const Login = () => {
       {/* bg */}
       <div className='h-screen w-screen overflow-hidden'>
         <div className='bg-black opacity-50 absolute w-full h-full'></div>
-        <img alt='l' src='https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_small.jpg'></img>
+        <img alt='h' src='https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_small.jpg'></img>
       </div>
 
       {/* header and login page */}
       <Header/>
-      <div className='h-[85vh] w-[70vh] top-20 left-[0.70vh] absolute bg-[#b03535d3]'>
+      <div className='h-[85vh] w-[70vh] top-20 left-[0.70vh] absolute bg-[#000000d3]'>
         <h1 className='text-white text-3xl font-bold mt-16 mx-10 mb-7'>{isSignInPage?'Sign In':'Sign up' }</h1>
         <form className="mx-2" onSubmit={(e)=>e.preventDefault()}>
           {!isSignInPage && <div tabIndex={0} className='group w-10/12 relative h-14 mx-8 my-4 bg-[#212121cc] border border-neutral-600 rounded-md focus-within:border-white focus-within:border-2'>
