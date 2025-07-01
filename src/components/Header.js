61150ae15a411c59e943b77a45cdf0dd2ec373ba
amanxsays <amanjhasahab07@gmail.com>
2025-07-01 22:09:33 +0530

    modifications

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
         <img alt="phot" className="h-8 md:h-16 " src={NETFLIX_LOGO}></img>
       </div>
       <div>
-        <button onClick={handleToggle} className="relative top-1.5 md:top-2 left-[30vh] md:left-[148vh] inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-600 to-black group-hover:from-red-600 group-hover:to-black hover:text-white dark:text-white  focus:outline-none dark:focus:ring-red">
+        <button onClick={handleToggle} className="relative top-1.5 md:top-2 left-[30vh] md:left-[144vh] inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-600 to-black group-hover:from-red-600 group-hover:to-black hover:text-white dark:text-white  focus:outline-none dark:focus:ring-red">
           <span className="relative px-0.5 md:px-5 md:py-2.5 transition-all ease-in duration-75 dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent group-hover:scale-125">
             {!showGpt?<span className="inline-block animate-[spin_3s_linear_infinite]"><SiOpenai className="scale-[80%] md:scale-[170%]"/></span>:<span className="inline-block animate-[bounce_1s_linear_infinite]"><FaHome className="md:scale-[190%] -mb-0.5"/></span>}
           </span>
@@ -46,7 +46,7 @@ const Header = () => {
         <img
           alt="_"
           src={selector?.photoURL}
-          className="rounded-md ml-[50%] md:ml-0 h-5 md:h-10 md:w-10"
+          className="rounded-md ml-[35%] md:ml-0 h-5 md:h-10 md:w-10"
         ></img>
         <div>
           <button
